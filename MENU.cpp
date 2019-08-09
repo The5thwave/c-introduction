f14918c8333fdb9e041e2e9706e068952df5160b
The5thwave <53308815+The5thwave@users.noreply.github.com>
2019-08-09 07:01:45 +0300

    Add files via upload

---
+++ MENU.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int pass;
+	menu://goto
+	cout<<"\t\t***WELCOME TO THE ATM***\n";
+	cout<<"\t\t***ENTER YOUR PASSWORD***\n";
+	cin>>pass;
+	pass==0000;
+	// checks if the password is correct (oooo)
+    if ( pass == 0000) 
+    {
+    cout << "CORRECT PASSWORD, PROCEED" << endl;
+    }
+    else
+    {
+    cout << "INCORRECT PASSWORD! TRY AGAIN" << endl;
+    }
+
+}
+
